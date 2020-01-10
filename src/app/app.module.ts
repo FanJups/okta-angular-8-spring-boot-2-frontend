4391d982f30c44443a751c958949ec737de62d0c
FanJups <jupsfan@gmail.com>
2020-01-10 17:48:10 +0100

    Generate a car-list component to display the list of cars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
+import { CarListComponent } from './car-list/car-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent
   ],
   imports: [
     BrowserModule,
