3c6e62bb32eb5d05a9a9d57ef2b6dc40a08ec2b7
FanJups <jupsfan@gmail.com>
2020-01-19 09:50:22 +0100

    Adding car list & car edit components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarListComponent } from './car-list/car-list.component';
+import { CarEditComponent } from './car-edit/car-edit.component';
 
 const routes: Routes = [];
 
