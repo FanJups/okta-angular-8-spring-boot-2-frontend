78774d8a1542629e48d6ba5b35174fa35612170d
FanJups <jupsfan@gmail.com>
2020-01-19 09:52:02 +0100

    Adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { CarListComponent } from './car-list/car-list.component';
 import { CarEditComponent } from './car-edit/car-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', redirectTo: '/car-list', pathMatch: 'full' },
+  {
+    path: 'car-list',
+    component: CarListComponent
+  },
+  {
+    path: 'car-add',
+    component: CarEditComponent
+  },
+  {
+    path: 'car-edit/:id',
+    component: CarEditComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
