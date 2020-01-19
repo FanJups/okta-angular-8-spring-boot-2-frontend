04cfbfeb0de1d84b69ca9566fb970241ddc29656
FanJups <jupsfan@gmail.com>
2020-01-19 09:54:25 +0100

    Adding new imports

--- src/app/car-edit/car-edit.component.ts
+++ src/app/car-edit/car-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarService } from '../shared/car/car.service';
+import { GiphyService } from '../shared/giphy/giphy.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-car-edit',
